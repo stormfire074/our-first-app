14705347b985d3103ae2df31dd5182e7f4d53e64
Syed Shoaib Ali Shah <shoaib@abit.ai>
2024-04-07 13:45:54 +0500

    save

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import "./App.css";
+import Header from "./Header";
 
+//BEM Block Element Modifier
 function App() {
   return (
-    <div className="App">
-      <h1>Hi </h1>
-      
-      
+    <div className="app">
+      <h1 className="app__title">This is a Title.</h1>
+      <Header/>
+
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1 className="header__title">This is the header</h1>
+      <h2 className="header__user">Shoaib is signed in.</h2>
+    </div>
+  )
+}
+
+export default Header
